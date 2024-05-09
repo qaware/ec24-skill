bf3f9d0e014faf032e8ec1556b7c80232cea7953
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2024-05-09 19:25:59 +0200

    Added skills business logic.

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# ec24-skill
-The EC24 Alexa skill.
+# The Engineering Camp Alexa Skill
+
+Simple Alexa demo skill to ask for the next talk in the Pretalx schedule.
+
+```bash
+# install CLI and create new skill project
+npm install -g ask-cli
+ask new
+
+# install dependencies
+cd lambda
+npm install
+npm install --save-dev ask-sdk-local-debug
+
+# to run and test implementation locally
+ask run --region EU
+ask dialog
+```
+
+Once you have everything up and running you can start with the development
+of the skill. Either use CoPilot or the following snippets to implement the
+actual business logic.
+
+```javascript
+// a function to get the complete EC24 schedule from Pretalx
+const https = require('https');
+function get(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            let data = '';
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                resolve(data);
+            });
+
+        }).on('error', (err) => {
+            reject(err);
+        });
+    });
+}
+
+// a function to check of the timestamp is today and in the future
+function isTodayAndAfterCurrentTime(timestamp) {
+    const now = new Date();
+    const timestampDate = new Date(timestamp);
+
+    return now.toISOString().slice(0,10) === timestampDate.toISOString().slice(0,10) && now.getTime() < timestampDate.getTime();
+}  
+```
+
+Then, add the `handle()` callback to your main intent handler. 
+Done. Deploy. Test.
+```javascript
+    async handle(handlerInput) {
+        let speakOutput = 'I could not find any talks for today. Please try again later.';
+
+        await get('https://ec24.qaware.de/api/events/ec24/talks/').then(
+            (data) => {
+                console.log(data);
+                const talks = JSON.parse(data);
+
+                for (let i = 0; i < talks.results.length; i++) {
+                    const talk = talks.results[i];
+                    const timestamp = talk.slot.start;
+                    if (isTodayAndAfterCurrentTime(timestamp)) {
+                        speakOutput = `The next talk is ${talk.title} by ${talk.speakers[0].name}`;
+                        break;
+                    }
+                }
+            }
+        ).catch(console.error);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt('Anything else I can do for you?')
+            .getResponse();
+    }
+```
+
+## Maintainer
+
+M.-Leander Reimer (@lreimer), <mario-leander.reimer@qaware.de>
+
+## License
+
+This software is provided under the GPL v3 open source license, read the `LICENSE` file for details.
---
+++ lambda/index.js
@@ -0,0 +1,200 @@
+/* *
+ * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+ * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * session persistence, api calls, and more.
+ * */
+const Alexa = require('ask-sdk-core');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Welcome, to Engineering Camp 2024. You can ask me for the next talk or ask for help!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const NextTalkIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'NextTalkIntent';
+    },
+    async handle(handlerInput) {
+        let speakOutput = 'I could not find any talks for today. Please try again later.';
+
+        await get('https://ec24.qaware.de/api/events/ec24/talks/').then(
+            (data) => {
+                console.log(data);
+                const talks = JSON.parse(data);
+
+                for (let i = 0; i < talks.results.length; i++) {
+                    const talk = talks.results[i];
+                    const timestamp = talk.slot.start;
+                    if (isTodayAndAfterCurrentTime(timestamp)) {
+                        speakOutput = `The next talk is ${talk.title} by ${talk.speakers[0].name}`;
+                        break;
+                    }
+                }
+            }
+        ).catch(console.error);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt('Anything else I can do for you?')
+            .getResponse();
+    }
+};
+
+function isTodayAndAfterCurrentTime(timestamp) {
+    const now = new Date();
+    const timestampDate = new Date(timestamp);
+
+    return now.toISOString().slice(0,10) === timestampDate.toISOString().slice(0,10) && now.getTime() < timestampDate.getTime();
+}
+
+const https = require('https');
+
+function get(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            let data = '';
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                resolve(data);
+            });
+
+        }).on('error', (err) => {
+            reject(err);
+        });
+    });
+}
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can aks for the next talk in the schedule.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        NextTalkIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .withCustomUserAgent('ec24-skill/v1.2')
+    .lambda();
